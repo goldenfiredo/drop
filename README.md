7168d87796df0137d1a8caa2499caedf313ffc49
goldenfiredo <goldenfiredo@gmail.com>
2021-10-07 14:12:26 +0800

    add script

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# drop
+## 向discord投放钱包地址 自动化脚本
+
+1. 安装node和git，要求nodejs版本不低于12.16.0 (git可以不安装, 直接下载zip文件)
+
+2. 从github上复制代码
+
+  ```
+  git clone git@github.com:goldenfiredo/drop.git
+  ```
+
+3. 安装运行脚本需要的依赖包
+
+  ```
+  npm install
+  ```
+
+4. 配置好你使用代理的端口号（科学上网）
+
+  ```
+  proxy: "http://127.0.0.1:7890",
+  ```
+
+  端口号7890按实际修改
+
+5. 如果你没有使用代理，跳过第4步，代码中做一下切换(两处)
+
+  ```
+  require("request").get(options, function (error, response, body) {
+  // requestProxy.get(options, function (error, response, body) {
+  ```
+
+  ```
+  require("request").post(options, function (error, response, body) {
+  // requestProxy.post(options, function (error, response, body) {
+  ```
+
+6. 编辑drop.txt文件, 加入你的discord帐号名称、密码和钱包地址，编辑好后drop.txt文件如下：
+  ```
+  discord帐号1|密码1|钱包地址1
+  discord帐号2|密码2|钱包地址2
+  discord帐号3|密码3|钱包地址3
+  discord帐号4|密码4|钱包地址4
+  discord帐号5|密码5|钱包地址5
+  discord帐号6|密码6|钱包地址6
+  discord帐号7|密码7|钱包地址7
+  discord帐号8|密码8|钱包地址8
+  discord帐号9|密码9|钱包地址9
+  discord帐号10|密码10|钱包地址10
+  ......
+  ```
+  如果想在命令行指定帐号的密码, 密码一列替换成*
+
+7. 以上discord帐号需要加入要投放钱包地址的项目方的discord
+
+8. 用其中的一个账号登录discord，在 用户设置 > 高级设置 里打开'开发者'模式，然后右键click你要投放地址的频道，弹出菜单里会有'复制ID'一项，点击它获得频道id
+
+9. 用如下命令运行脚本
+
+  ```
+  node drop.js 帐号密码或- 频道id 
+  ```
+  脚本依次登录drop.txt文件里帐号并向指定的频道发送相应的钱包地址.
+
+  * 如果drop.txt文件密码那一列有 *，命令行的第3项需要输入相应的帐号密码；如果密码在drop.txt里都已指定(即密码一列无 *)，这里用 - 占位即可
+
+10. 如果脚本在登录某账号时出现captcha required错误，目前没有好的解决方案，需要人工多次登录，直到discord不再要求captcha验证(?)
+
+11. 阅读脚本源码你有所收获或者运行脚本为你节省了时间，欢迎喂投, 钱包地址：0xb0ba5ceca5a2fac643febc2f9f3ac4a53162f384, 各链通用. 谢谢！
+
+![POAP bot](https://github.com/waxcloud2021/discord/blob/master/address.jpg)
+
+  * 0xb0ba5ceca5a2fac643febc2f9f3ac4a53162f384
---
+++ drop.js
@@ -0,0 +1,169 @@
+const fs = require('fs')
+
+const requestProxy = require("request").defaults({
+  proxy: "http://127.0.0.1:7890",
+  rejectUnauthorized: false,
+})
+
+const login_url = 'https://discord.com/api/v9/auth/login'
+const typing_url = 'https://discord.com/api/v9/channels/_id/typing'
+const messages_url = 'https://discord.com/api/v9/channels/_id/messages'
+const logout_url = 'https://discord.com/api/v9/auth/logout'
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+const user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+const MAGIC = 890383850297884672
+
+async function main() {
+  
+  if (process.argv.length < 4) {
+    console.log('node discord.js password|- channel_id')
+    return
+  }
+
+  let password1 = process.argv[2]
+  let channel_id = process.argv[3]
+
+  let list = fs.readFileSync('drop.txt')
+  let lines = list.toString().split('\n')
+  
+  for (let index in lines) {
+    let line = lines[index]
+    console.log(++index)
+    if (line.startsWith('#')) continue
+    let arr = line.split('|')
+    if (arr.length != 3) continue
+    let user = arr[0]
+    let password = arr[1]
+    let message = arr[2]
+    if (password == '*') {
+      if (password1 == '-') {
+        console.log('bad password')
+        continue
+      }
+      password = password1
+    }
+    
+
+    try {
+      await worker(user, password, channel_id, message)
+    } catch (err) {
+      console.log(err)
+    }
+
+    break
+  }
+}
+
+async function worker(user, password, poap_bot_channel_id, message) {
+
+  let headers = {
+    'user-agent': user_agent,
+  }
+
+  let params = {
+    "login": user,
+    "password": password,
+    "undelete": false,
+    "captcha_key": null,
+    "login_source": null,
+    "gift_code_sku_id": null
+  }
+
+  console.log('login')
+  let body = await synchronous_request('POST', login_url, params, headers)
+  if (body.captcha_key != undefined) {
+    console.log(body.captcha_key + '\n')
+    
+    return
+  }
+  
+  let token = body.token
+  if (token == undefined) {
+    console.log('login failed\n')
+
+    return
+  }
+
+  headers['authorization'] = token
+
+  let _typing_url = typing_url.replace('_id', poap_bot_channel_id)
+  await synchronous_request('POST', _typing_url, undefined, headers)
+  
+  await  wait(5000)
+  
+  console.log('send message ...')
+  let _messages_url = messages_url.replace('_id', poap_bot_channel_id)
+  let nonce = MAGIC + new Date().getTime() + Math.floor(Math.random() * 65535)
+  params = {
+    "content": message,
+    "nonce": nonce.toString(),
+    "tts": false
+  }
+
+  await synchronous_request('POST', _messages_url, params, headers)
+
+  console.log('logout\n')
+  params = {
+    "provider": null,
+    "voip_provider": null
+  }
+
+  try {
+    await synchronous_request('POST', logout_url, params, headers)
+  } catch (err) {
+    //
+  }
+
+  return
+} 
+
+// helper methods
+let synchronous_request = function (method, url, params, headers) {
+
+  let options = {
+    url: url,
+  }
+
+  if (method == 'GET') {
+    if (params != undefined) {
+      options['form'] = params
+    }
+    if (headers != undefined) {
+      options['headers'] = headers
+    }
+    
+    return new Promise(function (resolve, reject) {
+      // If you don't use proxy, require("request").get(...) is ok
+      // require("request").get(options, function (error, response, body) {
+      requestProxy.get(options, function (error, response, body) {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(body)
+            }
+        })
+    })
+  } else {
+    if (params != undefined) {
+      options['json'] = params
+    }
+    if (headers != undefined) {
+      options['headers'] = headers
+    }
+
+    return new Promise(function (resolve, reject) {
+      // require("request").post(options, function (error, response, body) {
+      requestProxy.post(options, function (error, response, body) {
+        if (error) {
+            reject(error)
+        } else {
+            resolve(body)
+        }
+      })
+    })
+  }
+}
+
+main()
+
